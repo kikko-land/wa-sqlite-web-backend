4b3d3361ab483319edf464995aebf66556013110
Sergey Popov <quolpr@gmail.com>
2022-07-14 14:40:08 +0300

    Add lint and typing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,11 @@
-const rootDir = process.cwd().includes("packages/wa-sqlite-web-backend")
-  ? "./"
-  : "./packages/wa-sqlite-web-backend";
-
 module.exports = {
-  extends: ["../common-scripts/eslintrc.cjs"],
+  extends: ["./node_modules/@trong-orm/common-scripts/eslintrc.cjs"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
     sourceType: "module",
-    tsconfigRootDir: rootDir,
     project: "./tsconfig.json",
   },
 };
